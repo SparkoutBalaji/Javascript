5d4d04fb993f55d774fdb4f377e802bf0ad75660
SparkoutBalaji <balaji.n@sparkouttech.com>
2023-09-12 11:49:41 +0530

    initial commit

--- basic/javascript.js
+++ basic/javascript.js
@@ -1,20 +1,21 @@
-// var string = "string"; //holds string value
-// var number = 13011;
-// var Boolean = true;
-// let undefined;
-// let numb = null;
-// var obj = { Name : "Balaji", Age : 23};
-// var arr = [1,2,3,4];
-// var regexp = RegExp(/^(A-Z){10}$/);
-// console.log(typeof string);
-// console.log(typeof number);
-// console.log(typeof Boolean);
-// console.log(typeof undefined);
-// console.log(typeof numb);
-// console.log(obj,typeof obj);
-// console.log(arr, typeof arr);
-// console.log(typeof regexp);
+var string = "string"; //holds string value
+var number = 13011;
+var Boolean = true;
+let undefined;
+let numb = null;
+var obj = { Name : "Balaji", Age : 23};
+var arr = [1,2,3,4];
+var regexp = RegExp(/^(A-Z){10}$/);
+console.log(typeof string);
+console.log(typeof number);
+console.log(typeof Boolean);
+console.log(typeof undefined);
+console.log(typeof un); // its return undefined because not declare this is a variable
+console.log(typeof numb);
+console.log(obj,typeof obj);
+console.log(arr, typeof arr);
+console.log(typeof regexp);
 
 
 let a = 10 >= 10;
-console.log(a);
+console.log(a); //return boolean value
